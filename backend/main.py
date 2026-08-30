b960e0904a327c4800500567ed5fc846b7006e36
pramahosting <pramahosting@gmail.com>
2026-08-30 10:56:18 +1000

    Add RedirectResponse import to main.py

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, RedirectResponse
 from contextlib import asynccontextmanager
 from sqlalchemy import text
 
